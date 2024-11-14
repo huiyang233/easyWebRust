1cf00a4928fe669bd934d853cdaa52485c8af4c3
yanghui <2409931477@qq.com>
2024-11-15 00:59:07 +0800

    change

--- sql/merchant_info.sql
+++ sql/merchant_info.sql
@@ -5,6 +5,8 @@ create table merchant_info
     name         varchar(100) null comment '商家的名字',
     audit_status int          null comment '审核状态 1=待审核 2=已通过 3=Ban',
     owner        bigint       null comment '老板的用户id',
+    agent_id     bigint       null comment '代理商的id',
+    city_id      bigint       null comment '城市id',
     is_del       bit          null,
     create_by    varchar(20)  null,
     update_by    varchar(20)  null,
