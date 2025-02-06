06b3726db534c1c3bd94affd9715b6fe79a0c9c8
yanghui <2409931477@qq.com>
2025-02-06 10:00:14 +0800

    添加测试请求的代码

--- src/api/common/sys_district_api.rs
+++ src/api/common/sys_district_api.rs
@@ -1,8 +1,9 @@
-use salvo::{handler, Request, Router};
-
-use crate::model::result::HttpPage;
+use crate::model::result::{Http, HttpPage, WebResult};
 use crate::model::sys_district::SysDistrictVo;
+use crate::model::user::SysUser;
 use crate::service::sys_district_service::SysDistrictService;
+use salvo::{handler, Request, Router};
+use salvo_core::Depot;
 
 pub fn init_sys_district_router() -> Router{
     Router::new()
@@ -10,11 +11,19 @@ pub fn init_sys_district_router() -> Router{
         .push(
             Router::with_path("sys_district")
                 .get(get_sys_district_by_page)
-        )
+        ) .push(
+        Router::with_path("test")
+            .get(test)
+    )
 }
 
 #[handler]
 pub async fn get_sys_district_by_page(req: &mut Request) ->HttpPage<SysDistrictVo>{
     SysDistrictService::get_sys_district_by_page(req).await
+}
+
+#[handler]
+pub async fn test(req: &mut Request,depot: &mut Depot) ->Http<SysUser>{
+    Ok(WebResult::success(SysUser::default()))
 }
 
